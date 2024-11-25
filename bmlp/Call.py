d4c68deac789723153afabc7f48b56fcc57790ea
Lun Ai <lun.ai15@imperial.ac.uk>
2024-11-25 01:08:04 +0000

    Allow calling BMLP-IE from python

--- bmlp/Call.py
+++ bmlp/Call.py
@@ -1,5 +1,7 @@
 from .Matrix import *
 
+# V and T are the input matrices, e.g. metabolite medium and gene knockout conditions
+
 
 def BMLP_IE_gpu(R1_path, R2_path, V_path, T_path, P_name, Res_path):
 
@@ -16,3 +18,16 @@ def BMLP_IE_gpu(R1_path, R2_path, V_path, T_path, P_name, Res_path):
 
     # Save computed result to result path
     boolean_matrix_to_integers(res, P_name, Res_path)
+
+
+# V and T are the input matrices, e.g. metabolite medium and gene knockout conditions
+def BMLP_IE_gpu(R1_path, R2_path, V, T):
+
+    # Load datasets stored in SuiteSparse binary format
+    R1 = R1_path
+    R2 = R2_path
+
+    # Run BMLP-IE
+    res, _ = BMLP_IE(V, R1, R2, T=T, localised=True)
+
+    return res

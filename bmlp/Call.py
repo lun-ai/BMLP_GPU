766d16fdcc55971ad4f8148ce6d1bf28f8992b91
Lun Ai <lun.ai15@imperial.ac.uk>
2024-11-26 11:49:39 +0000

    merge BMLP_IE multiple functions into one

--- bmlp/Call.py
+++ bmlp/Call.py
@@ -2,31 +2,24 @@ from .Matrix import *
 
 
 # V and T are the input matrices, e.g. metabolite medium and gene knockout conditions
-def BMLP_IE_gpu(R1_path, R2_path, V_path, T_path, P_name, Res_path):
+def BMLP_IE_gpu(R1_path, R2_path, V, T, P_name='', Res_path=''):
 
     # Load datasets stored in SuiteSparse binary format
     R1 = R1_path
     R2 = R2_path
 
     # Load inputs
-    V = integers_to_boolean_matrix(V_path)
-    T = integers_to_boolean_matrix(T_path)
+    if P_name != '' and Res_path != '':
+        V_m = integers_to_boolean_matrix(V)
+        T_m = integers_to_boolean_matrix(T)
 
-    # Run BMLP-IE
-    res, _ = BMLP_IE(V, R1, R2, T=T, localised=True)
+        # Run BMLP-IE
+        res, _ = BMLP_IE(V_m, R1, R2, T=T_m, localised=True)
 
-    # Save computed result to result path
-    boolean_matrix_to_integers(res, P_name, Res_path)
-
-
-# V and T are the input matrices, e.g. metabolite medium and gene knockout conditions
-def BMLP_IE_gpu(R1_path, R2_path, V, T):
-
-    # Load datasets stored in SuiteSparse binary format
-    R1 = R1_path
-    R2 = R2_path
-
-    # Run BMLP-IE
-    res, _ = BMLP_IE(V, R1, R2, T=T, localised=True)
-
-    return res
+        # Save computed result to result path
+        boolean_matrix_to_integers(res, P_name, Res_path)
+        return None
+    else:
+        # Run BMLP-IE
+        res, _ = BMLP_IE(V, R1, R2, T=T, localised=True)
+        return res
